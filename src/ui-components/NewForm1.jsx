7a230d9ce03985300fea151f29fa957381c23806
Edward J <edjeong99@gmail.com>
2023-09-21 22:23:22 -0500

    pull new form component from amplify

---
+++ src/ui-components/NewForm1.jsx
@@ -0,0 +1,264 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import {
+  Button,
+  Flex,
+  Grid,
+  SelectField,
+  TextField,
+} from "@aws-amplify/ui-react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { fetchByPath, validateField } from "./utils";
+import { API } from "aws-amplify";
+import { createExpense } from "../graphql/mutations";
+export default function NewForm1(props) {
+  const {
+    clearOnSuccess = true,
+    onSuccess,
+    onError,
+    onSubmit,
+    onValidate,
+    onChange,
+    overrides,
+    ...rest
+  } = props;
+  const initialValues = {
+    item: "",
+    category: "",
+    amount: "",
+  };
+  const [item, setItem] = React.useState(initialValues.item);
+  const [category, setCategory] = React.useState(initialValues.category);
+  const [amount, setAmount] = React.useState(initialValues.amount);
+  const [errors, setErrors] = React.useState({});
+  const resetStateValues = () => {
+    setItem(initialValues.item);
+    setCategory(initialValues.category);
+    setAmount(initialValues.amount);
+    setErrors({});
+  };
+  const validations = {
+    item: [{ type: "Required" }],
+    category: [],
+    amount: [{ type: "Required" }],
+  };
+  const runValidationTasks = async (
+    fieldName,
+    currentValue,
+    getDisplayValue
+  ) => {
+    const value =
+      currentValue && getDisplayValue
+        ? getDisplayValue(currentValue)
+        : currentValue;
+    let validationResponse = validateField(value, validations[fieldName]);
+    const customValidator = fetchByPath(onValidate, fieldName);
+    if (customValidator) {
+      validationResponse = await customValidator(value, validationResponse);
+    }
+    setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }));
+    return validationResponse;
+  };
+  return (
+    <Grid
+      as="form"
+      rowGap="15px"
+      columnGap="15px"
+      padding="20px"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        let modelFields = {
+          item,
+          category,
+          amount,
+        };
+        const validationResponses = await Promise.all(
+          Object.keys(validations).reduce((promises, fieldName) => {
+            if (Array.isArray(modelFields[fieldName])) {
+              promises.push(
+                ...modelFields[fieldName].map((item) =>
+                  runValidationTasks(fieldName, item)
+                )
+              );
+              return promises;
+            }
+            promises.push(
+              runValidationTasks(fieldName, modelFields[fieldName])
+            );
+            return promises;
+          }, [])
+        );
+        if (validationResponses.some((r) => r.hasError)) {
+          return;
+        }
+        if (onSubmit) {
+          modelFields = onSubmit(modelFields);
+        }
+        try {
+          Object.entries(modelFields).forEach(([key, value]) => {
+            if (typeof value === "string" && value === "") {
+              modelFields[key] = null;
+            }
+          });
+          await API.graphql({
+            query: createExpense,
+            variables: {
+              input: {
+                ...modelFields,
+              },
+            },
+          });
+          if (onSuccess) {
+            onSuccess(modelFields);
+          }
+          if (clearOnSuccess) {
+            resetStateValues();
+          }
+        } catch (err) {
+          if (onError) {
+            const messages = err.errors.map((e) => e.message).join("\n");
+            onError(modelFields, messages);
+          }
+        }
+      }}
+      {...getOverrideProps(overrides, "NewForm1")}
+      {...rest}
+    >
+      <TextField
+        label="Item"
+        isRequired={true}
+        isReadOnly={false}
+        value={item}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              item: value,
+              category,
+              amount,
+            };
+            const result = onChange(modelFields);
+            value = result?.item ?? value;
+          }
+          if (errors.item?.hasError) {
+            runValidationTasks("item", value);
+          }
+          setItem(value);
+        }}
+        onBlur={() => runValidationTasks("item", item)}
+        errorMessage={errors.item?.errorMessage}
+        hasError={errors.item?.hasError}
+        {...getOverrideProps(overrides, "item")}
+      ></TextField>
+      <SelectField
+        label="Category"
+        placeholder="Please select an option"
+        isDisabled={false}
+        value={category}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              item,
+              category: value,
+              amount,
+            };
+            const result = onChange(modelFields);
+            value = result?.category ?? value;
+          }
+          if (errors.category?.hasError) {
+            runValidationTasks("category", value);
+          }
+          setCategory(value);
+        }}
+        onBlur={() => runValidationTasks("category", category)}
+        errorMessage={errors.category?.errorMessage}
+        hasError={errors.category?.hasError}
+        {...getOverrideProps(overrides, "category")}
+      >
+        <option
+          children="Entertainment"
+          value="Entertainment"
+          {...getOverrideProps(overrides, "categoryoption0")}
+        ></option>
+        <option
+          children="Food"
+          value="Food"
+          {...getOverrideProps(overrides, "categoryoption1")}
+        ></option>
+        <option
+          children="Beauty"
+          value="Beauty"
+          {...getOverrideProps(overrides, "categoryoption2")}
+        ></option>
+        <option
+          children="ETC"
+          value="ETC"
+          {...getOverrideProps(overrides, "categoryoption3")}
+        ></option>
+      </SelectField>
+      <TextField
+        label="Amount"
+        isRequired={true}
+        isReadOnly={false}
+        type="number"
+        step="any"
+        value={amount}
+        onChange={(e) => {
+          let value = isNaN(parseFloat(e.target.value))
+            ? e.target.value
+            : parseFloat(e.target.value);
+          if (onChange) {
+            const modelFields = {
+              item,
+              category,
+              amount: value,
+            };
+            const result = onChange(modelFields);
+            value = result?.amount ?? value;
+          }
+          if (errors.amount?.hasError) {
+            runValidationTasks("amount", value);
+          }
+          setAmount(value);
+        }}
+        onBlur={() => runValidationTasks("amount", amount)}
+        errorMessage={errors.amount?.errorMessage}
+        hasError={errors.amount?.hasError}
+        {...getOverrideProps(overrides, "amount")}
+      ></TextField>
+      <Flex
+        justifyContent="space-between"
+        {...getOverrideProps(overrides, "CTAFlex")}
+      >
+        <Button
+          children="Clear"
+          type="reset"
+          onClick={(event) => {
+            event.preventDefault();
+            resetStateValues();
+          }}
+          {...getOverrideProps(overrides, "ClearButton")}
+        ></Button>
+        <Flex
+          gap="15px"
+          {...getOverrideProps(overrides, "RightAlignCTASubFlex")}
+        >
+          <Button
+            children="Submit"
+            type="submit"
+            variation="primary"
+            isDisabled={Object.values(errors).some((e) => e?.hasError)}
+            {...getOverrideProps(overrides, "SubmitButton")}
+          ></Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  );
+}
--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,6 +4,6 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
-export { default as ExpenseCreateForm } from "./ExpenseCreateForm";
 export { default as ExpenseUpdateForm } from "./ExpenseUpdateForm";
+export { default as NewForm1 } from "./NewForm1";
 export { default as studioTheme } from "./studioTheme";
